76c404fa342b755684e20f91ce209c050744dc59
sirbuig <sirbuig@proton.me>
2024-04-26 18:27:47 +0300

    feat: mandelbrot set bmp generated

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -1,1 +1,41 @@
+#include <malloc.h>
 #include "../include/mandelbrot.h"
+#include "../include/rgb.h"
+
+#define MIN_X -2.00
+#define MAX_X 0.47
+#define MIN_Y -1.12
+#define MAX_Y 1.12
+#define MAX_ITERATION 1000
+
+void mandelbrot(BMP_Image *img) {
+    RGB *palette = (RGB *) malloc(MAX_ITERATION * sizeof(RGB));
+    for (unsigned int c = 0; c < MAX_ITERATION; c++) {
+        palette[c].red = (unsigned char) (255 * c / MAX_ITERATION);
+        palette[c].green = 125;
+        palette[c].blue = (unsigned char) (255 * (MAX_ITERATION - c) / MAX_ITERATION);
+    }
+
+    for (unsigned int i = 0; i < img->height; i++) {
+        for (unsigned int j = 0; j < img->width; j++) {
+            double x0 = (j / (double)img->width) * (MAX_X - MIN_X) + MIN_X;
+            double y0 = (i / (double)img->height) * (MAX_Y - MIN_Y) + MIN_Y;
+
+            double x, y, aux;
+            unsigned int iteration = 0;
+
+            x = y = 0.0;
+            while (x * x + y * y <= 4 && iteration < MAX_ITERATION) {
+                aux = x * x - y * y + x0;
+                y = 2 * x * y + y0;
+                x = aux;
+                iteration += 1;
+            }
+
+            RGB color = palette[(iteration == MAX_ITERATION) ? MAX_ITERATION - 1 : iteration];
+            img->data[i * img->width + j] = color;
+        }
+    }
+
+    free(palette);
+}
